14caec22a27ccfcd940249f2c6be8ca4b6023ada
Adrian De Andrade <adriandeandrade98@gmail.com>
2018-11-08 20:29:43 -0500

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        GameObject paddle = other.gameObject.GetComponent<PaddleController>().gameObject;
+
+        if(paddle)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    private Ball ball;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(new Vector3(x, 0f, 0));
+
+        CheckBounds();
+    }
+
+    private void CheckBounds()
+    {
+        if(transform.position.x >= 12)
+        {
+            transform.position = new Vector3(12f, transform.position.y, 0f);
+        }
+
+        if (transform.position.x <= -12)
+        {
+            transform.position = new Vector3(-12f, transform.position.y, 0f);
+        }
+    }
+}
